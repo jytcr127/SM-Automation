8518705f6b3097d6175c1f3f42d2f37e44603287
jytcr127 <jyt1279@gmail.com>
2020-06-10 18:22:03 -0600

    Add additional tests 5 and 6 to test ability to search for music video

--- main-test-page.rb
+++ main-test-page.rb
@@ -2,6 +2,7 @@ require 'selenium-webdriver'
 require 'webdrivers'
 require 'rspec'
 require_relative 'SearchSections'
+require 'capybara/rspec'
 driver = Selenium::WebDriver.for :chrome
 persons = SearchSections.new(driver)
 
@@ -33,3 +34,18 @@ end
          expect(groupname).to eql('NCT Musicians')
     end
 end
+
+    describe persons do
+    it 'Test 5: Search for a music video' do
+        musicvideosection = persons.searchMusicVideo
+        expect(musicvideosection).to eql ('Music Video')
+    end
+end
+
+describe persons do 
+    it 'Test 6:  Music video selected should load' do
+        playvideo = persons.loadMusicVideo
+        expect(playvideo).to eql (true)
+    end
+end
+
--- searchSections.rb
+++ searchSections.rb
@@ -1,5 +1,18 @@
 require 'page-object'
 
+#test 5 variables
+$optionsMenu = '/html/body/div[2]/div[1]/div/div/a[2]'
+$musicVideoOption = '/html/body/div[2]/div[1]/div/div/div[2]/ul/li[1]/a'
+
+#test 6 variables
+$groupDropdownButton = '/html/body/div/div[2]/div[1]/div[2]/div/div[2]/ul/li/div/a'
+$groupToClick = '/html/body/div/div[2]/div[1]/div[2]/div/div[2]/ul/li/div/div/div[1]/div[6]/a'
+$musicVideoToClick = '/html/body/div/div[2]/div[1]/div[2]/div/ul/li[1]/a/span[1]/span'
+$musicVideoLoaded = '/html/body/div/div[2]/div[2]'
+
+
+
+
 class SearchSections 
 
     include PageObject
@@ -7,7 +20,6 @@ class SearchSections
     def searchMusicians
     driver = Selenium::WebDriver.for :chrome
     driver.navigate.to 'https://www.smtown.com/'
-    sleep 2
    driver.find_element(xpath:'//*[@id="header"]/div/div/ul/li[1]/a').click
     headliner = driver.find_element(xpath: '/html/body/div/div[2]/div/div[2]/div/div[1]/div/h2').text
     return headliner
@@ -16,8 +28,7 @@ class SearchSections
     def searchActors
     driver = Selenium::WebDriver.for :chrome
     driver.navigate.to 'https://www.smtown.com/'
-    sleep 2
-   driver.find_element(xpath:'//*[@id="header"]/div/div/ul/li[2]/a').click
+    driver.find_element(xpath:'//*[@id="header"]/div/div/ul/li[2]/a').click
     headliner = driver.find_element(xpath: '/html/body/div/div[2]/div/div[2]/div/div[1]/div/h2').text
     return headliner
     end
@@ -25,8 +36,7 @@ class SearchSections
     def searchEntertainers
     driver = Selenium::WebDriver.for :chrome
     driver.navigate.to 'https://www.smtown.com/'
-    sleep 2
-   driver.find_element(xpath:'//*[@id="header"]/div/div/ul/li[3]/a').click
+    driver.find_element(xpath:'//*[@id="header"]/div/div/ul/li[3]/a').click
     headliner = driver.find_element(xpath: '/html/body/div/div[2]/div/div[2]/div/div[1]/div/h2').text
     return headliner
     end
@@ -34,11 +44,31 @@ class SearchSections
     def searchGroup
     driver = Selenium::WebDriver.for :chrome
     driver.navigate.to 'https://www.smtown.com/'
-    sleep 2
     driver.find_element(class: 'btn_search').click
     driver.find_element(class: 'input_search').send_keys('nct')
     driver.find_element(class: 'input_search').send_keys :return
     groupname = driver.find_element(class: 'title').text
     return groupname
     end
+
+    def searchMusicVideo
+    driver = Selenium::WebDriver.for :chrome
+    driver.navigate.to 'https://www.smtown.com/'
+    driver.find_element(xpath: $optionsMenu).click
+    driver.find_element(xpath: $musicVideoOption).click
+    headertitle = driver.find_element(class: 'title2').text 
+    return headertitle
+    end
+
+    def loadMusicVideo
+    driver = Selenium::WebDriver.for :chrome
+    driver.navigate.to 'https://www.smtown.com/mv'
+    driver.find_element(xpath: $groupDropdownButton).click
+    wait = Selenium::WebDriver::Wait.new(timeout: 50) # seconds
+    wait.until { driver.find_element(xpath: $groupToClick).displayed?}
+    driver.find_element(xpath: $groupToClick).click
+    driver.find_element(xpath: $musicVideoToClick).click
+    return driver.find_element(xpath: $musicVideoLoaded).displayed?
+    end
 end
+
